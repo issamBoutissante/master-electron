41239897c5435afe0d8a55f3d8cc1af9280700bf
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-11-20 10:45:09 +0100

    have practices two apis

--- src/index.js
+++ src/index.js
@@ -3,37 +3,43 @@ const path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
   app.quit();
 }
+let mainWindow;
+let messageWindow;
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: "hidden",
   });
-  let messageWindow = new BrowserWindow({
+  messageWindow = new BrowserWindow({
     width: 300,
     height: 200,
     parent: mainWindow,
-    Frame: false,
-    modal: true,
+    show: false,
+    frame: false,
   });
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "index.html"));
   messageWindow.loadFile(path.join(__dirname, "./simpleMessage.html"));
-  // Open the DevTools.
+  console.log(app.getName());
   //mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", (e) => {
+    mainWindow = null;
+  });
+  messageWindow.on("closed", (e) => {
+    messageWindow = null;
+  });
+  app.on("browser-window-focus", (event, window) => {
+    if (mainWindow) messageWindow.show();
+  });
+  app.on("browser-window-blur", (event, window) => {
+    if (mainWindow) messageWindow.hide();
+  });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
@@ -41,12 +47,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
